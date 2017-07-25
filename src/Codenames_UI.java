edaf65b416b998e9048f9ba327b2bd801d47d10e
dawbyrd <dawsonbyrd561@gmail.com>
2017-07-24 20:35:06 -0700

    update user interface

--- src/Codenames_UI.java
+++ src/Codenames_UI.java
@@ -1,14 +1,12 @@
-import java.io.*;
-import java.lang.reflect.Array;
-import java.net.URL;
-import java.util.*;
-
 import Jama.Matrix;
-import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
-import javafx.util.Pair;
+
+import java.io.*;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
 import static java.lang.Math.exp;
-import static java.lang.Math.max;
 
 /**
  * Created by dawsonbyrd on 7/20/17.
@@ -16,7 +14,7 @@ import static java.lang.Math.max;
 
 //9 one color, 8 another, 1 assasin, 7 bystanders
 
-public class Codenames {
+public class Codenames_UI {
 
     final static float[] B = new float[]{-3.55106049399243f, 11.8332877194120f};
     final static float t = 0.75f;
@@ -29,7 +27,7 @@ public class Codenames {
     static ArrayList<String> words = new ArrayList<>();
     static ArrayList<String> opp = new ArrayList<>();
     static ArrayList<String> bystanders = new ArrayList<>();
-    static String assasin;
+    static String assassin;
 
     static ArrayList<Hint> maximums = new ArrayList<>();
 
@@ -37,11 +35,7 @@ public class Codenames {
 
     public static void main(String[] args) throws IOException {
 
-        FileWriter fw = new FileWriter("performance.txt", true);
-        BufferedWriter bw = new BufferedWriter(fw);
-        PrintWriter out = new PrintWriter(bw);
-
-        System.out.print("\rretrieving word vectors...");
+        System.out.println("\rretrieving word vectors...");
         util.getVectors(100000);
         Scanner input = new Scanner(System.in);
 
@@ -52,95 +46,55 @@ public class Codenames {
         //URL url = new URL("https://raw.githubusercontent.com/jbowens/codenames/master/assets/words.txt");
         Scanner s = new Scanner(url.openStream());
 
-        System.out.print("\rdownloading word set...");
+        System.out.println("\rdownloading word set...");
         int i=0;
+
         while(s.hasNext()){data[i] = s.next().toLowerCase(); i++;}
 
-        Scanner in = new Scanner(System.in);
+        File file = new File("board.txt");
+        Scanner in = new Scanner(file);
 
-        while(true) {
-            words.clear(); opp.clear(); maximums.clear();
+        System.out.println("loading board...");
+        words.clear(); opp.clear(); maximums.clear();
 
-            System.out.println("\rgenerating game board...");
+        int num_words = Integer.parseInt(in.nextLine());
+        for(int k=0; k<num_words; k++){
+            String curr = in.next();
+            words.add(curr);}
 
-            int n = 0;
-            while (n < data.length) {
-                if (opp.size() == 8 && words.size() == 8) break;
+        int num_opp = Integer.parseInt(in.next());
+        for(int k=0; k<num_opp; k++){
+            String curr = in.next();
+            opp.add(curr);}
 
-                double rand = Math.random();
-                if (rand < 0.05) {
-                    if (util.getVec(data[n]) == null) continue;
-                    if (data[n].length() < 3) continue;
+        int num_by = Integer.parseInt(in.next());
+        for(int k=0; k<num_by; k++){bystanders.add(in.next());}
 
-                    if (rand < 0.025) {
-                        if (words.size() < 8) words.add(data[n]);
-                    } else if (opp.size() < 8) {
-                        opp.add(data[n]);
-                    }
-                }
+        assassin = in.next();
 
-                if (n == data.length - 1) {
-                    n = 0;
-                    opp.clear();
-                    words.clear();
-                } else n++;
-            }
+        System.out.println("getting hint...");
 
-            ArrayList<String> cards = new ArrayList<>();
-            cards.addAll(words);
-            cards.addAll(opp);
-            Collections.shuffle(cards);
+        for (int k = 1; k<=num_words; k++) {
+            count = 0;
+            maximums.add(new Hint(0, "", new int[]{0}));
 
-            for (int j = 0; j < 4; j++) {
-                for (int x = 0; x < 3; x++) {
-                    System.out.print(cards.get(4 * j + x) + " ");
-                }
-                System.out.println(cards.get(4 * j + 3));
-            }
+            int[] subset = new int[k];
+            checkSubsets(num_words, subset, 0, 0);
 
-            for (int k = 2; k <= 8; k++) {
-                count = 0;
-                maximums.add(new Hint(0, "", new int[]{0}));
-
-                int[] subset = new int[k];
-                checkSubsets(8, subset, 0, 0);
-
-                if (maximums.get(k - 2).prob < 0.4) break;
-            }
-
-
-            System.out.println();
-            //System.out.println(maximums);
-
-            int shift = (maximums.size() > 1) ? 2 : 1;
-            Hint final_hint = maximums.get(maximums.size() - shift);
-            System.out.println("hint: " + final_hint.word + "," + final_hint.s.length);
-
-            for (int j = 0; j < final_hint.s.length; j++) {
-                System.out.print("pick " + j + ":");
-                String pick = in.nextLine();
-                if (!words.contains(pick)) {
-                    System.out.println("\rincorrect!");
-                    out.print(0);break;
-                }
-                if (j == final_hint.s.length - 1){System.out.println("\rcorrect!"); out.print(1);}
-            }
-
-            String[] intendend = new String[final_hint.s.length];
-
-            for (int j = 0; j < intendend.length; j++) {
-                intendend[j] = words.get(final_hint.s[j]);
-            }
-
-            System.out.println("intended cards: " + Arrays.toString(intendend));
-
-            System.out.print("quit:"); String response = in.nextLine();
-            if(response.equals("y")) break;
-
-            System.out.println();
+            if (maximums.get(k - 1).prob < 0.4) break;
         }
 
-        out.close();
+        System.out.println();
+        //System.out.println(maximums);
+
+        int shift = (maximums.size() > 1) ? 2 : 1;
+        Hint final_hint = maximums.get(maximums.size() - shift);
+        System.out.println("hint: " + final_hint.word + "," + final_hint.s.length);
+
+        String[] intendend = new String[final_hint.s.length];
+        for (int j = 0; j < intendend.length; j++) {intendend[j] = words.get(final_hint.s[j]);}
+
+        System.out.println("intended cards: " + Arrays.toString(intendend));
     }
 
     public static float prob(float sim){
@@ -180,16 +134,22 @@ public class Codenames {
                     if(curr_prob<min_prob) min_prob=curr_prob;
                 }
 
-                float max_prob=0;
+                float max_prob_opp=0; float max_prob_by=0;
+
                 for(String s: opp){
                     float curr_prob = prob(util.cosineSimilarity(hint, util.getVec(s)));
-                    if(curr_prob>max_prob){max_prob = curr_prob;}
-                }
+                    if(curr_prob>max_prob_opp){max_prob_opp = curr_prob;}}
 
-                if(max_prob<0.1 /*t*min_prob*/ && prob > maximums.get(subsetSize-2).prob){
+                for(String s: bystanders){
+                    float curr_prob = prob(util.cosineSimilarity(hint, util.getVec(s)));
+                    if(curr_prob>max_prob_opp){max_prob_by = curr_prob;}}
+
+                float ass_prob = prob(util.cosineSimilarity(hint, util.getVec(assassin)));
+
+                if(ass_prob<0.1 && max_prob_opp<0.2 && max_prob_by<0.3 && prob > maximums.get(subsetSize-1).prob){
                     int[] k = new int[subsetSize];
                     for(int m=0; m<subsetSize; m++){k[m]=subset[m];}
-                    maximums.set(subsetSize-2, new Hint(prob, curr_word, k));
+                    maximums.set(subsetSize-1, new Hint(prob, curr_word, k));
                 }
             }
 
